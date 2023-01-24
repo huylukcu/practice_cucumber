234109f4961002ca32a4691fdd86cb8c84c1c883
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-23 18:17:50 -0600

    first commit

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,23 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        dryRun = false,
+        tags = "@parametrizing1"
+)
+public class Runner {
+}
+/*
+ features = "./src/test/resources/features",//PATH OF FEATURES FOLDER
+ glue = "stepdefinitions", //PATH OF STEP DEFINITIONS
+ dryRun = false //IF dryRun = true. RUN DRY. JUST GIVE ME THE MISSING STEP DEFINITIONS. DON'T RUN THE TEST CASES.
+ dryRun= false when we are doing test execution. dryRun=true when we are generation missing step definitions
+When you run Runner, it will go to feature file, and start execution from top to bottom.
+        If there is any step matching, Java will find that matching 'stepdefinition', and it will be executed.
+        If there is no match then Cucumber will generate template for us in the console.
+        */
